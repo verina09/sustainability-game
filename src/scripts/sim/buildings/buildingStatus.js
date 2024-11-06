756bc1f6c4fae799aa7533918c122bf1b22ac120
verina09 <verinacristie@gmail.com>
2024-11-06 13:31:57 +0300

    Remove all files

--- src/scripts/sim/buildings/buildingStatus.js
+++
@@ -1,5 +0,0 @@
-export const BuildingStatus = {
-  NoPower: 'no-power',
-  NoRoadAccess: 'no-road-access',
-  Ok: 'ok'
-}
--- src/scripts/sim/buildings/buildingType.js
+++
@@ -1,8 +0,0 @@
-export const BuildingType = {
-  residential: 'residential',
-  commercial: 'commercial',
-  industrial: 'industrial',
-  road: 'road',
-  powerPlant: 'power-plant',
-  powerLine: 'power-line'
-}
